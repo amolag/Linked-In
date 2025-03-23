d9c8fbb66e22bf65cf88c8ed2549ded63a4953c8
AMOL <amolag1674@gmail.com>
2025-03-23 21:07:43 +0530

    A small error is fixed

--- Backend/server.js
+++ Backend/server.js
@@ -38,10 +38,10 @@ app.use("/api/v1/notifications", notificationRoutes);
 app.use("/api/v1/connections", connectionRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
   });
 }
 
